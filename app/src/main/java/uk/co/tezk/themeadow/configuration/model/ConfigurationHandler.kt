4372eae7ab482baa67c42a009c22aba978c4ace9
terrykay <tel@tezk.co.uk>
2018-10-10 16:13:21 +0100

    Update ConfigurationHandler.kt

--- app/src/main/java/uk/co/tezk/themeadow/configuration/model/ConfigurationHandler.kt
+++ app/src/main/java/uk/co/tezk/themeadow/configuration/model/ConfigurationHandler.kt
@@ -4,18 +4,19 @@ import android.util.Log
 import retrofit2.HttpException
 import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
+import uk.co.tezk.themeadow.configuration.model.MeadowConfig
 import uk.co.tezk.themeadow.preferences.MeadowPreferences
 import uk.co.tezk.themeadow.repository.MeadowRetrofit
 import javax.net.ssl.SSLHandshakeException
 
 class ConfigurationHandler {
-    fun retrieveConfig(meadowClient: MeadowRetrofit, callBack: (() -> Unit)? = null) {
+    fun retrieveConfig(meadowClient: MeadowRetrofit, callBack: ((meadowConfig : MeadowConfig) -> Unit)? = null) {
         meadowClient.getConfig()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe({ meadowConfig ->
                     MeadowPreferences.getInstance().set(MeadowPreferences.CALENDAR_URL, meadowConfig.calendarUrl)
-                    callBack?.let { it() }
+                    callBack?.let { it(meadowConfig) }
                 }, {
 
                     when (it) {
